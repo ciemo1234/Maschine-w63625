77d817970f9ff32c0c596459a9bcfbfa5d879a33
ciemo1234 <51116685+ciemo1234@users.noreply.github.com>
2020-07-12 10:58:33 +0200

    dodane

---
+++ MachineTest.java
@@ -0,0 +1,26 @@
+package core;
+
+import junit.framework.TestCase;
+
+import products.Product;
+
+import java.util.List;
+
+public class MachineTest extends TestCase {
+
+    public void testAddProduct() {
+
+
+        Machine machine = new Machine();
+        Product product1 = new Product(),
+                product2 = new Product(),
+                product3 = new Product();
+
+        machine.addProduct(product1);
+        machine.addProduct(product2);
+        machine.addProduct(product3);
+
+        List<Product> products = machine.getProducts();
+        System.out.println(products.size());
+    }
+}
